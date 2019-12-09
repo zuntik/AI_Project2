b1a302e742c855aaf3e6b1a266038595f050c4cf
simão soares <simaosoares@MacBook-Pro-4.local>
2019-12-09 23:10:11 +0000

    still errors :(

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ if __name__=="__main__":
 
 from probability import *
 from itertools import product
-from math import comb
 
 class Problem:
 
@@ -45,7 +44,7 @@ class Problem:
                 for room, adjacent in self.graph.items():
                     first_half = {i:1.0 for j,i in enumerate(product((True,False),repeat=len(adjacent)+1)) if j < 2**len(adjacent) }
                     second_half = {i: self.P for j,i in enumerate(product((True,False),repeat=len(adjacent)+1)) if j >= 2**len(adjacent) }
-                    second_half[(F,F,F)] = 0
+                    second_half[(F,)*(len(adjacent)+1)] = 0
                     print((room+'_'+str(t), str('_'+str(t-1)+' ').join([room]+adjacent)+'_'+str(t-1), {**first_half, **second_half} ))
                     self.bn.add((room+'_'+str(t), str('_'+str(t-1)+' ').join([room]+adjacent)+'_'+str(t-1), {**first_half, **second_half} ))
 
@@ -53,13 +52,16 @@ class Problem:
                 print((s+'_'+str(t), self.sensors[s]['room']+'_'+str(t), {T:self.sensors[s]['tpr'], F: self.sensors[s]['fpr']}))
                 self.bn.add((s+'_'+str(t), self.sensors[s]['room']+'_'+str(t), {T:self.sensors[s]['tpr'], F: self.sensors[s]['fpr']}))
 
-    def construct_evidence(self):
-        self.ev = {s+'_'+str(t):val for t,ts in enumerate(self.time_stamps) for s,val in ts.items()}
+    def construct_ev(self):
+        to_bool = lambda  x: T if x=='T' else F
+        self.ev = {s+'_'+str(t):to_bool(val) for t,ts in enumerate(self.time_stamps) for s,val in ts.items()}
+        print(self.ev)
 
     def solve(self):
         self.construct_net()
         self.construct_ev()
         results = ( (room, elimination_ask(room+'_'+str(len(self.time_stamps)-1),self.ev,self.bn)[True]) for room in self.graph )
+        print(list(results))
         return max(results, key=lambda i: i[1])
         #return (room, likelihood)
 
@@ -67,6 +69,6 @@ def solver(input_file):
     return Problem(input_file).solve()
 
 if __name__=="__main__":
-    fp = open("examples2\P2.txt")
+    fp = open("examples2/P2.txt")
     # solved files should finish with *_solved.txt
-    solver(fp)
+    print(solver(fp))
