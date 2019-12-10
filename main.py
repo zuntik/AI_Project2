a6e669ca784a6726f9c6abedeacf272c316f7d63
Thomas Berry <thomasdpberry@tecnico.ulisboa.pt>
2019-12-10 01:33:33 +0000

    works with test files but not with example files

--- main.py
+++ main.py
@@ -37,38 +37,38 @@ class Problem:
 
         for t, ts in enumerate(self.time_stamps):
             if t == 0:
+                # on the first time stamp the rooms have no parents
                 for room in self.graph:
-                    print((room + '_' + str(t), '', 0.5))
-                    self.bn.add((room+'_'+str(t), '', 0.5))
+                    self.bn.add((room+'_0', '', 0.5))
             else:
+                # for the following timestamps the
                 for room, adjacent in self.graph.items():
                     first_half = {i:1.0 for j,i in enumerate(product((True,False),repeat=len(adjacent)+1)) if j < 2**len(adjacent) }
                     second_half = {i: self.P for j,i in enumerate(product((True,False),repeat=len(adjacent)+1)) if j >= 2**len(adjacent) }
                     second_half[(F,)*(len(adjacent)+1)] = 0
-                    print((room+'_'+str(t), str('_'+str(t-1)+' ').join([room]+adjacent)+'_'+str(t-1), {**first_half, **second_half} ))
                     self.bn.add((room+'_'+str(t), str('_'+str(t-1)+' ').join([room]+adjacent)+'_'+str(t-1), {**first_half, **second_half} ))
 
+            # the sensors only depend on the current timestamp
             for s in ts:
-                print((s+'_'+str(t), self.sensors[s]['room']+'_'+str(t), {T:self.sensors[s]['tpr'], F: self.sensors[s]['fpr']}))
                 self.bn.add((s+'_'+str(t), self.sensors[s]['room']+'_'+str(t), {T:self.sensors[s]['tpr'], F: self.sensors[s]['fpr']}))
 
     def construct_ev(self):
         to_bool = lambda  x: T if x=='T' else F
         self.ev = {s+'_'+str(t):to_bool(val) for t,ts in enumerate(self.time_stamps) for s,val in ts.items()}
-        print(self.ev)
 
     def solve(self):
         self.construct_net()
         self.construct_ev()
-        results = ( (room, elimination_ask(room+'_'+str(len(self.time_stamps)-1),self.ev,self.bn)[True]) for room in self.graph )
-        print(list(results))
-        return max(results, key=lambda i: i[1])
-        #return (room, likelihood)
+        return max(((room, elimination_ask(room+'_'+str(len(self.time_stamps)-1),self.ev,self.bn)[True]) for room in self.graph), key=lambda i: i[1])
 
 def solver(input_file):
     return Problem(input_file).solve()
 
 if __name__=="__main__":
-    fp = open("examples2/P2.txt")
-    # solved files should finish with *_solved.txt
-    print(solver(fp))
+    from glob import glob
+    for l in open("public_tests/solutions.txt"):
+        print(l)
+        l = l.split()
+        sol = solver(open("public_tests/"+l[0]))
+        print(l[0] +' '+ sol[0]+ ' ' + str(sol[1]) + ' mine' +'\n')
+
